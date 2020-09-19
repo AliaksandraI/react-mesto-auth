1e4c739178ea153d9d493337efcfe8adb746b171
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-09-19 16:46:57 +0200

    Updates

---
+++ precache-manifest.5084c9b45343daaa3764ce1077d249ef.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f20baa5d0a99c03db86cd49f4f51227",
+    "url": "/react-mesto-auth/index.html"
+  },
+  {
+    "revision": "cc572764f36e33f38a81",
+    "url": "/react-mesto-auth/static/css/main.49c11412.chunk.css"
+  },
+  {
+    "revision": "61a62f3c0c77ade148eb",
+    "url": "/react-mesto-auth/static/js/2.3f71270b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mesto-auth/static/js/2.3f71270b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc572764f36e33f38a81",
+    "url": "/react-mesto-auth/static/js/main.22bbb5f1.chunk.js"
+  },
+  {
+    "revision": "bbb93e786b9494524bbb",
+    "url": "/react-mesto-auth/static/js/runtime-main.b8dd5a43.js"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/react-mesto-auth/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "1a1fd467638016c60144ca826ffab8d1",
+    "url": "/react-mesto-auth/static/media/Inter-Bold.1a1fd467.ttf"
+  },
+  {
+    "revision": "e7ae98681edfa1df7f1e3ebba0d4fb88",
+    "url": "/react-mesto-auth/static/media/Inter-Bold.e7ae9868.woff2"
+  },
+  {
+    "revision": "fc6aff6f40099ca7468f88df9122ae31",
+    "url": "/react-mesto-auth/static/media/Inter-Bold.fc6aff6f.otf"
+  },
+  {
+    "revision": "073dabe9ba6f45f0913873d6810648d1",
+    "url": "/react-mesto-auth/static/media/Inter-ExtraBold.073dabe9.ttf"
+  },
+  {
+    "revision": "33ddc61574b7fa7df6258a3489fe7e5c",
+    "url": "/react-mesto-auth/static/media/Inter-ExtraBold.33ddc615.otf"
+  },
+  {
+    "revision": "a0e89d60e007ea22dad528c5dec09cd4",
+    "url": "/react-mesto-auth/static/media/Inter-ExtraBold.a0e89d60.woff2"
+  },
+  {
+    "revision": "b82eef497995d28fffc2ef185211e282",
+    "url": "/react-mesto-auth/static/media/Inter-ExtraBold.b82eef49.woff"
+  },
+  {
+    "revision": "33bcc59c5a5f480212806c7c5ecedbd5",
+    "url": "/react-mesto-auth/static/media/Inter-Light.33bcc59c.otf"
+  },
+  {
+    "revision": "732be9e43cdbbadb84847dbc4b99d0ad",
+    "url": "/react-mesto-auth/static/media/Inter-Light.732be9e4.ttf"
+  },
+  {
+    "revision": "a1f9e860d918b33aa82a0c2c10d30d6f",
+    "url": "/react-mesto-auth/static/media/Inter-Light.a1f9e860.woff2"
+  },
+  {
+    "revision": "c58ea231468353018b9f8ca321f18282",
+    "url": "/react-mesto-auth/static/media/Inter-Light.c58ea231.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/react-mesto-auth/static/media/add__button.b2fce4ac.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/react-mesto-auth/static/media/bad_registration.4072126c.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/react-mesto-auth/static/media/close_button.5df32a06.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/react-mesto-auth/static/media/delete_button.d8078343.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "/react-mesto-auth/static/media/edit__button.b05083e5.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/react-mesto-auth/static/media/good_registration.e38b395c.svg"
+  },
+  {
+    "revision": "a8acc871456b1001597672e02c5199ca",
+    "url": "/react-mesto-auth/static/media/heart.a8acc871.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/react-mesto-auth/static/media/heart_active.5bbb05f7.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/react-mesto-auth/static/media/logo.c6f11019.svg"
+  },
+  {
+    "revision": "908c20cb9256d92925c68f1708ee9781",
+    "url": "/react-mesto-auth/static/media/not_found.908c20cb.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/react-mesto-auth/static/media/pen.d476a8b8.svg"
+  },
+  {
+    "revision": "d6ed763994daf9a66161772350f830fd",
+    "url": "/react-mesto-auth/static/media/гора_эльбрус.d6ed7639.jpg"
+  }
+]);
